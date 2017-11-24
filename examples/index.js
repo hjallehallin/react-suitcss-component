4ee203fbc7d6604162b2c9a19073254171de03a9
hjalle <hjalle@earthpeople.se>
2017-11-24 18:05:10 +0100

    Add examples

--- examples/index.js
+++ examples/index.js
@@ -1,1 +1,18 @@
-// soon
+import React, { Component } from 'react'
+import { Button } from './Button'
+
+class Example extends Component {
+  render () {
+    return (
+      <div>
+        <h2>Buttons</h2>
+        <p><Button>Plain button</Button></p>
+        <p><Button modifier='primary'>Primary button</Button></p>
+        <p><Button is='active'>Active button</Button></p>
+        <p><Button disabled>Disabled button</Button> disabled prop gets passed native button-element</p>
+      </div>
+    )
+  }
+}
+
+export default Example
